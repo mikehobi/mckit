dcdb14b3b084d57553bf4335dc4ffbd4a613bf80
mikehobi <mikehobizal@gmail.com>
2019-04-30 17:09:02 -0700

    boom

---
+++ mckit/McKit.h
@@ -0,0 +1,19 @@
+//
+//  McKit.h
+//  McKit
+//
+//  Created by Mike Hobizal on 4/30/19.
+//  Copyright © 2019 Mike Hobizal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for McKit.
+FOUNDATION_EXPORT double McKitVersionNumber;
+
+//! Project version string for McKit.
+FOUNDATION_EXPORT const unsigned char McKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <McKit/PublicHeader.h>
+
+
---
+++ mckit/McView.swift
@@ -0,0 +1,20 @@
+//
+//  McView.swift
+//  McKit
+//
+//  Created by Mike Hobizal on 4/30/19.
+//  Copyright © 2019 Mike Hobizal. All rights reserved.
+//
+
+import UIKit
+
+class McView: UIView {
+    init() {
+        super.init(frame: .zero)
+        self.backgroundColor = .blue
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
