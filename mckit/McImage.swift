4b2ba4c2e47e68a6c3341cbeecad95bc349e814f
mikehobi <mikehobizal@gmail.com>
2019-05-02 15:53:34 -0700

    New McImage WIP

---
+++ mckit/McImage.swift
@@ -0,0 +1,12 @@
+//
+//  McImage.swift
+//  McKit
+//
+//  Created by Mike Hobizal on 5/2/19.
+//  Copyright © 2019 Mike Hobizal. All rights reserved.
+//
+
+import UIKit
+
+open class McImage: UIImage {
+}
