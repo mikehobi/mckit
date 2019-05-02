985001f3e0091db523d6e7f5334df92fcda8d53b
mikehobi <mikehobizal@gmail.com>
2019-05-02 15:49:41 -0700

    Update McView, add McLabel

--- mckit/McLabel.swift
+++ mckit/McLabel.swift
@@ -1,14 +1,14 @@
 //
-//  McView.swift
+//  McLabel.swift
 //  McKit
 //
-//  Created by Mike Hobizal on 4/30/19.
+//  Created by Mike Hobizal on 5/2/19.
 //  Copyright © 2019 Mike Hobizal. All rights reserved.
 //
 
 import UIKit
 
-open class McView: UIView {
+open class McLabel: UILabel {
     public init() {
         super.init(frame: .zero)
         self.backgroundColor = .red
